79d593b4062961066a2149f3085efa79861332ec
Valentin <valen.arrascaeta@gmail.com>
2020-05-12 18:31:47 -0300

    a

--- dict.c
+++ dict.c
@@ -11,7 +11,7 @@ struct _node_t {
 
 dict_t dict_empty() {
     dict_t dict = NULL;
-    dict = (dict_t)malloc(sizeof(struct _node_t));
+    dict = (dict_t)malloc(sizeof(struct _node_t));//a
     assert(dict != NULL && dict_length(dict) == 0);
     return dict;
 }
